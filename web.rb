994391792cd22542c071e9f97817f29fa9626de2
Delvis Taveras <delvistaveras@cmu-413690.wv.cc.cmu.edu>
2013-09-15 05:25:21 -0400

    delvis

--- web.rb
+++ web.rb
@@ -1,13 +1,11 @@
 require 'rubygems'
 require 'sinatra'
 require 'twilio-ruby'
-puts "hello"
 get '/hello-monkey' do
   Twilio::TwiML::Response.new do |r|
-     purple = r.Say :voice => "woman" do |m|
+     r.Say :voice => "woman" do |m|
        m.body "ouchy ouchy ouchy"
      end
-     puts "#{purple.inspect}"
   end
 end
 
