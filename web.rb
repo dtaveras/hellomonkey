32717220ae401cdc00a65d29cd7fbfdd70885def
Delvis Taveras <delvistaveras@cmu-413690.wv.cc.cmu.edu>
2013-09-15 03:48:45 -0400

    test voice attr

--- web.rb
+++ web.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'twilio-ruby'
  
 get '/hello-monkey' do
-  Twilio::TwiML::Response.new do |r|
-    r.Say voice => "woman", 'Hello Monkey'
+  Twilio::TwiML::Response.new :voice => "woman" do |r|
+    r.Say 'Hello Monkey'
   end.text
 end
