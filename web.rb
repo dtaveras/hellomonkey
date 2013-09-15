2a7f6dfa1b41403b6eb7ad6e618e8086e38d2667
Delvis Taveras <delvistaveras@cmu-413690.wv.cc.cmu.edu>
2013-09-15 04:52:40 -0400

    lasjdf

--- web.rb
+++ web.rb
@@ -3,9 +3,14 @@ require 'sinatra'
 require 'twilio-ruby'
  
 get '/hello-monkey' do
+=begin
   Twilio::TwiML::Response.new do |r|
      r.Say :voice => "woman" do |m|
        m.body "ouchy ouchy ouchy"
     end
   end.text
+=end
+Twilio::TwiML::Response.new do |r|
+     r.Say "Hello Delvis youre the king"
 end
+end.text
